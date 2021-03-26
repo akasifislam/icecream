657a77b592f2accfb77f09816fa2f8584fbb14e2
akasifislam <aseasifislam@gmail.com>
2021-03-27 01:15:21 +0600

    factory file generate

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,9 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Category;
+use App\Post;
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -23,5 +26,21 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'cat_name' => $faker->name,
+    ];
+});
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'cat_id' => rand(1, 100),
+        'user_id' => rand(1, 100),
+        'comment_id' => rand(1, 100),
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'photo' => $faker->imageUrl,
+
     ];
 });
--- resources/views/admin/adminmaster.blade.php
+++ resources/views/admin/adminmaster.blade.php
@@ -17,31 +17,6 @@
 <div class="wrapper">
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="../../index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-    </ul>
-
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fa fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form>
-
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Notifications Dropdown Menu -->
@@ -81,15 +56,6 @@
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    {{-- <a href="../../index3.html" class="brand-link">
-      <img src="{{ asset('asset/admin/default/logo.jpg') }}"
-           alt="AdminLTE Logo"
-           class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a> --}}
-
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user (optional) -->
@@ -140,8 +106,15 @@
             <a href="../widgets.html" class="nav-link">
               <i class="nav-icon fa fa-th"></i>
               <p>
-                Widgets
-                <span class="right badge badge-danger">New</span>
+                Category
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../widgets.html" class="nav-link">
+              <i class="nav-icon fa fa-th"></i>
+              <p>
+                Post
               </p>
             </a>
           </li>
