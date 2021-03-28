4d59f3c3d1b5d192df3a4e6bc60a7650cde7845d
akasifislam <aseasifislam@gmail.com>
2021-03-28 21:22:17 +0600

    adsfhdsj

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,14 @@
 import AdminHome from './components/admin/AdminHome.vue'
+import Ex from './components/ExampleComponent.vue'
 
 
 export const routes = [
     {
         path: '/home',
         component: AdminHome 
-    }
+    },
+    {
+        path: '/post',
+        component: Ex
+    },
 ]
